8a329357722dcdae5456a5691aaf68b55b6449ab
WJLV0425 <weliferlv@hotmail.com>
2020-10-19 15:21:44 -0400

    Update Cacatuo.R

---
+++ Cacatuo.R
@@ -0,0 +1,10 @@
+# Área de cargar paquetes lol
+library(vegan)
+library(tidyverse)
+library(sf)
+source('biodata/funciones.R')
+
+# Área de cargar datos 
+# Mi familia de matas "Fabaceae-mimosoideae"
+load('')
+load('biodata/matriz_ambiental.Rdata') #Matriz ambiental, se carga como "bci_env_grid"
