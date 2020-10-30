ff3ce62c25a053c71ccb70e823ed1dac144a9a61
WJLV0425 <weliferlv@hotmail.com>
2020-10-30 16:33:52 -0400

    Cacatuo on fire

--- Cacatuo.R
+++ Cacatuo.R
@@ -16,6 +16,58 @@ library(RColorBrewer)
 load('biodata/Fabaceae-mimosoideae.Rdata')
 load('biodata/matriz_ambiental.Rdata') #Matriz ambiental, se carga como "bci_env_grid"
 
+
+#' ### Imprimir datos en pantalla (impresiones parciales con head)
+head(censo_fbc_m)
+head(mc_fbc_m)
+bci_env_grid # No necesita imprimirse parcialmente
+
+#' ### También podemos usar
+#' Requiere que se haya cargado ya la colección tidyverse
+censo_fbc_m %>% tibble
+mc_fbc_m %>% tibble
+
+#' ### Lista de especies
+sort(colnames(mc_fbc_m))
+
+#' ### Número de sitios, tanto en matriz de comunidad como en ambiental
+#' Verifica que coinciden
+nrow(mc_fbc_m) #En la matriz de comunidad
+nrow(bci_env_grid) #En la matriz ambiental
+
+#' ### Riqueza numérica de especies (usando matriz de comunidad) por quadrat
+#' Nota: cargar paquete vegan arriba, en el área de paquetes
+specnumber(mc_fbc_m)
+sort(specnumber(mc_fbc_m)) # Ordenados ascendentemente
+summary(specnumber(mc_fbc_m)) # Resumen estadístico
+
+#' ### Abundancia de especies por quadrat
+sort(rowSums(mc_fbc_m))
+summary(rowSums(mc_fbc_m)) # Resumen estadístico
+
+#' ### Abundancia por especie
+sort(colSums(mc_fbc_m))
+summary(colSums(mc_fbc_m)) # Resumen estadístico
+
+#' ### Riqueza numérica de toda la "comunidad"
+specnumber(colSums(mc_fbc_m))
+
+#' ### Abundancia de toda la comunidad
+sum(colSums(mc_fbc_m))
+
+#' ### Una tabla para el manuscrito, es necesario asignarle nombre
+#' Para esto, usaré la colección "tidyverse"
+abun_sp <- censo_fbc_m %>%
+  group_by(Latin) %>% 
+  count() %>% 
+  arrange(desc(n))
+abun_sp
+
+#' ### Un gráfico para el manuscrito
+#' Gráfico de mosaicos de la abundancia por especie por cuadros
+abun_sp_q <- crear_grafico_mosaico_de_mc(mc_fbc_m, tam_rotulo = 6)
+abun_sp_q
+
 #' ### Generar mapa de cuadros sin simbología
 mapa_cuadros <- mapView(
   bci_env_grid,
@@ -71,14 +123,14 @@ mapa_cuadros_riq_global %>% mapshot(file = 'mapa_cuadros_riq_global.png')
 
 #' ### Mapa de cuadros, simbología por abundancia de mi familia
 mapa_cuadros_abun_mi_familia <- mapView(
-  bci_env_grid %>% mutate(abun = rowSums(mc_apcyn_melic_saptc)),
+  bci_env_grid %>% mutate(abun = rowSums(mc_fbc_m)),
   layer.name = 'abundancia',
   alpha.regions = 0.6,
   map.types = 'OpenTopoMap',
   legend = T, zoom = 14,
   col.regions = azul,
   zcol = 'abun') %>%
-  addStaticLabels(label = rowSums(mc_apcyn_melic_saptc)) %>%
+  addStaticLabels(label = rowSums(mc_fbc_m)) %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
@@ -88,17 +140,18 @@ mapa_cuadros_abun_mi_familia %>% mapshot(file = 'mapa_cuadros_abun_mi_familia.pn
 
 #' ### Mapa de cuadros, simbología por riqueza de mi familia
 mapa_cuadros_riq_mi_familia <- mapView(
-  bci_env_grid %>% mutate(riq = specnumber(mc_apcyn_melic_saptc)),
+  bci_env_grid %>% mutate(riq = specnumber(mc_fbc_m)),
   layer.name = 'riqueza',
   alpha.regions = 0.6,
   map.types = 'OpenTopoMap',
   legend = T, zoom = 14,
   col.regions = rojo,
   zcol = 'riq') %>%
-  addStaticLabels(label = specnumber(mc_apcyn_melic_saptc)) %>%
+  addStaticLabels(label = specnumber(mc_fbc_m)) %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
     zoom = 15)
 mapa_cuadros_riq_mi_familia
 mapa_cuadros_riq_mi_familia %>% mapshot(file = 'mapa_cuadros_riq_mi_familia.png')
+
