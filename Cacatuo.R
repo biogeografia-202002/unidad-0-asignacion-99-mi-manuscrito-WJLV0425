e953cb898da30203e0ed2fcd742a887de576f58b
WJLV0425 <weliferlv@hotmail.com>
2020-12-29 09:26:38 -0400

    Si

--- Cacatuo.R
+++ Cacatuo.R
@@ -155,3 +155,259 @@ mapa_cuadros_riq_mi_familia <- mapView(
 mapa_cuadros_riq_mi_familia
 mapa_cuadros_riq_mi_familia %>% mapshot(file = 'mapa_cuadros_riq_mi_familia.png')
 
+
+
+#VARIABLES AMBIENTALES GLOBALES: PENDIENTE, NITRÓGENO, pH. "MAPAS_aed_4"
+
+
+#' ### Cargar paquetes
+library(mapview)
+library(tidyverse)
+library(sf)
+library(RColorBrewer)
+
+#' ### Cargar datos
+load('biodata/matriz_ambiental.Rdata')
+
+#' ### Paletas
+azul <- colorRampPalette(brewer.pal(8, "Blues"))
+rojo <- colorRampPalette(brewer.pal(8, "Reds"))
+rojo_inv <- colorRampPalette(rev(brewer.pal(8, "Reds")))
+
+#' ### Mapa de cuadros, simbología por pendiente
+mapa_cuadros_pendiente <- mapView(
+  bci_env_grid,
+  layer.name = 'pendiente',
+  alpha.regions = 0.4,
+  map.types = 'OpenTopoMap',
+  legend = T, zoom = 14,
+  col.regions = rojo,
+  zcol = 'pendiente_media') %>%
+  addStaticLabels(label = round(bci_env_grid$pendiente_media, 1)) %>%
+  leaflet::setView(
+    lng = -79.85136,
+    lat = 9.15097,
+    zoom = 15)
+mapa_cuadros_pendiente
+mapa_cuadros_pendiente %>% mapshot(file = 'mapa_cuadros_pendiente.png') #Genera archivo
+
+#' ### Mapa de cuadros, simbología por Nitrógeno
+mapa_cuadros_nit <- mapView(
+  bci_env_grid,
+  layer.name = 'N (mg/kg)',
+  alpha.regions = 0.4,
+  map.types = 'OpenTopoMap',
+  legend = T, zoom = 14,
+  col.regions = rojo,
+  zcol = 'N') %>%
+  addStaticLabels(label = round(bci_env_grid$N, 1)) %>%
+  leaflet::setView(
+    lng = -79.85136,
+    lat = 9.15097,
+    zoom = 15)
+mapa_cuadros_nit
+mapa_cuadros_nit %>% mapshot(file = 'mapa_cuadros_nit.png')
+
+#' ### Mapa de cuadros, simbología por pH
+mapa_cuadros_ph <- mapView(
+  bci_env_grid,
+  layer.name = 'pH',
+  alpha.regions = 0.4,
+  map.types = 'OpenTopoMap',
+  legend = T, zoom = 14,
+  col.regions = rojo_inv,
+  zcol = 'pH') %>%
+  addStaticLabels(label = round(bci_env_grid$pH, 1)) %>%
+  leaflet::setView(
+    lng = -79.85136,
+    lat = 9.15097,
+    zoom = 15)
+mapa_cuadros_ph
+mapa_cuadros_ph %>% mapshot(file = 'mapa_cuadros_ph.png')
+
+
+
+
+##CORRELACIONES_VARIABLES_AMBIENTALES (aed_5)
+
+
+knitr::opts_chunk$set(fig.width=12, fig.height=8)
+
+#' ### Cargar paquetes
+library(tidyverse)
+library(sf)
+library(ez)
+library(psych)
+library(vegan)
+
+#' ### Cargar datos
+load('biodata/matriz_ambiental.Rdata')
+load('biodata/Fabaceae-mimosoideae.Rdata')
+
+#' ### Una correlación simple
+cor(bci_env_grid$pendiente_media, bci_env_grid$geomorf_vertiente_pct)
+plot(bci_env_grid$pendiente_media, bci_env_grid$geomorf_vertiente_pct)
+cor.test(bci_env_grid$pendiente_media, bci_env_grid$geomorf_vertiente_pct)
+
+#' ### Generar objeto de columnas numéricas
+#' El objeto que generaré, denominado `env_num`, no tendrá las columnas `id` y las de coordenadas UTM, y añadiré la abundancia y riqueza de mi familia. Al mismo tiempo, insertaré un enter (`\n`) en nombres largos de variables, para acomodar los nombres de variables al panel de correlaciones; por ejemplo, el nombre `riqueza_global` se renombra a `riqueza\nglobal`.
+env_num <- bci_env_grid %>%
+  dplyr::select_if(is.numeric) %>%
+  dplyr::select(-id, -matches('^U.*')) %>% 
+  st_drop_geometry %>% 
+  mutate(
+    riqueza_mifam = specnumber(mc_fbc_m),
+    abundancia_mifam = rowSums(mc_fbc_m)) %>% 
+  rename_all(gsub, pattern = '_pct$', replacement = '') %>% 
+  rename_all(gsub, pattern = '_| ', replacement = '\n')
+env_num %>% tibble
+
+#' ### Panel de correlaciones con herramientas del paquete `graphics` y `psych`
+cor(env_num)
+ncol(env_num)
+pairs(env_num[,sample(1:33, 15)]) # paquete graphics
+env_num[,sample(1:33, 15)] %>% pairs.panels #paquete psych
+
+#' ### Panel de correlaciones con `ez`
+#' 
+#' #### Todas las variables (se empasta). Comentado, sólo mostrado para fines didácticos
+# p_cor_todos <- env_num %>%
+#   ezCor(r_size_lims = c(4,8), label_size = 4)
+# p_cor_todos
+
+#' #### Sólo suelo (elementos y pH), abundancia/riqueza
+p_cor_suelo_ar <- env_num %>%
+  dplyr::select(matches('^[A-T,Z]|abundancia|riqueza|^pH$', ignore.case = F)) %>%
+  ezCor(r_size_lims = c(4,8), label_size = 3)
+p_cor_suelo_ar
+
+#' #### Sólo heterogeneidad, geomorfologia, abundancia/riqueza
+p_cor_geomorf_ar <- env_num %>%
+  dplyr::select(-matches('^[A-T,Z]|pH', ignore.case = F)) %>%
+  ezCor(r_size_lims = c(4,8), label_size = 3)
+p_cor_geomorf_ar
+
+#' #### Matriz de comunidad
+p_cor_mc <- mc_fbc_m %>%
+  rename_all(gsub, pattern = '_| ', replacement = '\n') %>% 
+  ezCor(r_size_lims = c(4,8), label_size = 3)
+p_cor_mc
+
+
+
+## MAPAS POR LOTES (aed_6)
+
+knitr::opts_chunk$set(fig.width=12, fig.height=8)
+
+#' ## Preámbulo
+#' 
+#' ### Cargar paquetes
+#' 
+library(tmap)
+library(sf)
+library(tidyverse)
+library(RColorBrewer)
+#' 
+#' ### Cargar datos
+#' 
+load('biodata/matriz_ambiental.Rdata')
+#' 
+#' ## Convertir a KML
+#' 
+# st_write(
+#  bci_env_grid %>% rename(Name = id),
+# driver = 'KML',
+# dsn = 'matriz_ambiental.kml')
+# st_write(
+# bci_env_grid %>% rename(Name = id) %>% st_centroid(),
+# driver = 'KML',
+# dsn = 'matriz_ambiental_puntos.kml')
+#' 
+#' Uní los dos archivos anteriores en un único KML nombrado como `mapa_cuadros_1ha_para_google_earth.kml`, el cual muestra los puntos como rótulos identificadores de cada cuadro de 1 Ha dentro de sus correspondientes polígonos. Visualizar dicho archivo en GoogleEarth para un "recorrido virtual" por BCI.
+#' 
+#' ## Generar mapas por lotes
+#' 
+#' ### Variables ambientales numéricas con `ggplot2`
+#' 
+mapas_var_amb_num_gg <- bci_env_grid %>%
+  select_if(is.numeric) %>% 
+  gather(variable, valor, -geometry) %>% 
+  group_by(variable) %>% 
+  mutate(
+    valor = scales::rescale(valor, to = c(0, 1)),
+    id = rep(1:50)) %>% 
+  ggplot +
+  aes(geometry = geometry, fill = valor) +
+  theme(axis.text = element_blank()) +
+  geom_sf(lwd = 0.1, color = 'grey50', alpha = 0.8) + coord_sf() +
+  scale_fill_gradientn(colours = brewer.pal(11, 'BrBG')) +
+  geom_sf_text(aes(label = id, color = between(valor, 0.3, 0.7)), size = 1.75) +
+  scale_color_manual(guide = FALSE, values = c("white", "black")) +
+  facet_wrap(~ variable, ncol = 6) + 
+  ggtitle('Cuadros de 1 Ha de BCI. Variables ambientales numéricas escaladas de 0 a 1')
+mapas_var_amb_num_gg
+#'
+#' PNG
+#'
+png(
+  filename = 'mapas_variables_ambientales_numericas.png',
+  width = 1700, height = 1080, res = 150)
+mapas_var_amb_num_gg
+dev.off()
+#' 
+#' ### Variables ambientales numéricas con `tmap`
+#' 
+mapas_var_amb_num_tmap <- bci_env_grid %>%
+  select_if(is.numeric) %>% 
+  gather(variable, valor, -geometry) %>% 
+  group_by(variable) %>% 
+  mutate(
+    valor = scales::rescale(valor, to = c(0, 1)),
+    id = rep(1:50)) %>% 
+  tm_shape() +
+  tm_polygons(col = 'valor',
+              palette = brewer.pal(11, 'BrBG'),
+              style ='cont',
+              legend.is.portrait = FALSE) +
+  tm_facets(by = 'variable', ncol = 6, nrow = 6) +
+  tm_layout(main.title="Cuadros de 1 Ha de BCI. Variables ambientales numéricas escaladas de 0 a 1",
+            main.title.size = 0.7,
+            legend.outside.position="bottom",
+            legend.outside=TRUE,
+            legend.width = 0.2,
+            legend.text.size = 0.5,
+            legend.stack="horizontal", 
+            outer.margins=0)
+mapas_var_amb_num_tmap
+#'
+#' PNG
+#' 
+png(
+  filename = 'mapas_variables_ambientales_numericas_tmap.png',
+  width = 1800, height = 1400, res = 350, pointsize = 12)
+mapas_var_amb_num_tmap
+dev.off()
+#' 
+#' ### Variables ambientales nominales con `tmap`
+#' 
+mapas_var_amb_nom_tmap <- bci_env_grid %>%
+  select_if(negate(is.numeric)) %>% 
+  gather(variable, valor, -geometry) %>% 
+  tm_shape() +
+  tm_polygons(col = 'valor',
+              palette = brewer.pal(8, 'Set1'),
+              legend.show = T) +
+  tm_facets(by = 'variable', ncol = 2, free.scales = T, free.coords = T) +
+  tm_layout(main.title="Cuadros de 1 Ha de BCI. Variables ambientales nominales",
+            main.title.size = 0.7,
+            asp = 3.5,
+            legend.text.size = 0.7)
+mapas_var_amb_nom_tmap
+#'
+#' PNG
+#' 
+png(
+  filename = 'mapas_variables_ambientales_nominales_tmap.png',
+  width = 2000, height = 1200, res = 350, pointsize = 12)
+mapas_var_amb_nom_tmap
+dev.off()
