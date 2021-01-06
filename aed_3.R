654dec573b249595226cae9a801418915290dba1
WJLV0425 <weliferlv@hotmail.com>
2021-01-06 15:53:45 -0400

    2D

--- aed_3.R
+++ aed_3.R
@@ -1,24 +1,32 @@
-library(vegan)
-library(tidyverse)
-library(sf)
+#Paquetes
+
 library(mapview)
+library(sf)
+library(tidyverse)
+library(vegan)
 library(RColorBrewer)
 source('biodata/funciones.R')
 
+#Cargar datos
 
+load('biodata/Fabaceae-mimosoideae.Rdata')
+load('biodata/matriz_ambiental.Rdata') #Matriz ambiental, se carga como "bci_env_grid"
+
+#Explorar el objeto de matriz ambiental
+bci_env_grid
 
 #' ### Generar mapa de cuadros sin simbología
 mapa_cuadros <- mapView(
   bci_env_grid,
   col.regions = 'grey80',
-  alpha.regions = 0.3,
+  alpha.regions = 0.9,
   map.types = 'OpenTopoMap',
-  legend = F, zoom = 14,
+  legend = F, zoom = 10,
   zcol = 'id') %>% addStaticLabels() %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
-    zoom = 16)
+    zoom = 15)
 mapa_cuadros
 mapa_cuadros %>% mapshot(file = 'mapa_cuadros.png') #Genera archivo
 
@@ -30,16 +38,16 @@ rojo <- colorRampPalette(brewer.pal(8, "Reds"))
 mapa_cuadros_abun_global <- mapView(
   bci_env_grid,
   layer.name = 'abundancia',
-  alpha.regions = 0.6,
+  alpha.regions = 0.9,
   map.types = 'OpenTopoMap',
-  legend = T, zoom = 14,
+  legend = T, zoom = 10,
   col.regions = azul,
   zcol = 'abundancia_global') %>%
   addStaticLabels(label = bci_env_grid$abundancia_global, textsize = "7pt") %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
-    zoom = 15)
+    zoom = 15.5)
 mapa_cuadros_abun_global
 mapa_cuadros_abun_global %>% mapshot(file = 'mapa_cuadros_abun_global.png') 
 
@@ -47,16 +55,16 @@ mapa_cuadros_abun_global %>% mapshot(file = 'mapa_cuadros_abun_global.png')
 mapa_cuadros_riq_global <- mapView(
   bci_env_grid,
   layer.name = 'riqueza',
-  alpha.regions = 0.6,
+  alpha.regions = 0.9,
   map.types = 'OpenTopoMap',
-  legend = T, zoom = 14,
+  legend = T, zoom = 10,
   col.regions = rojo,
   zcol = 'riqueza_global') %>%
   addStaticLabels(label = bci_env_grid$riqueza_global, textsize = "7pt") %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
-    zoom = 15)
+    zoom = 15.5)
 mapa_cuadros_riq_global
 mapa_cuadros_riq_global %>% mapshot(file = 'mapa_cuadros_riq_global.png')
 
@@ -64,16 +72,16 @@ mapa_cuadros_riq_global %>% mapshot(file = 'mapa_cuadros_riq_global.png')
 mapa_cuadros_abun_mi_familia <- mapView(
   bci_env_grid %>% mutate(abun = rowSums(mc_fbc_m)),
   layer.name = 'abundancia',
-  alpha.regions = 0.6,
+  alpha.regions = 0.9,
   map.types = 'OpenTopoMap',
-  legend = T, zoom = 14,
+  legend = T, zoom = 10,
   col.regions = azul,
   zcol = 'abun') %>%
   addStaticLabels(label = rowSums(mc_fbc_m)) %>%
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
-    zoom = 15)
+    zoom = 15.5)
 mapa_cuadros_abun_mi_familia
 mapa_cuadros_abun_mi_familia %>% mapshot(file = 'mapa_cuadros_abun_mi_familia.png')
 
@@ -81,7 +89,7 @@ mapa_cuadros_abun_mi_familia %>% mapshot(file = 'mapa_cuadros_abun_mi_familia.pn
 mapa_cuadros_riq_mi_familia <- mapView(
   bci_env_grid %>% mutate(riq = specnumber(mc_fbc_m)),
   layer.name = 'Riqueza',
-  alpha.regions = 0.6,
+  alpha.regions = 0.9,
   map.types = 'OpenTopoMap',
   legend = T, zoom = 10,
   col.regions = rojo,
@@ -90,7 +98,7 @@ mapa_cuadros_riq_mi_familia <- mapView(
   leaflet::setView(
     lng = -79.85136,
     lat = 9.15097,
-    zoom = 16.5)
+    zoom = 15.5)
 mapa_cuadros_riq_mi_familia
 mapa_cuadros_riq_mi_familia %>% mapshot(file = 'mapa_cuadros_riq_mi_familia.png')
 
