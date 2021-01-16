91ab88bf60c93557b3442cd91d62d8fe3e4f041b
WJLV0425 <weliferlv@hotmail.com>
2021-01-16 14:20:27 -0400

    DD16_2

--- Cacatuo.R
+++ Cacatuo.R
@@ -65,7 +65,7 @@ abun_sp
 
 #' ### Un gráfico para el manuscrito
 #' Gráfico de mosaicos de la abundancia por especie por cuadros
-abun_sp_q <- crear_grafico_mosaico_de_mc(mc_fbc_m, tam_rotulo = 6)
+abun_sp_q <- crear_grafico_mosaico_de_mc(mc_fbc_m, tam_rotulo = 12)
 abun_sp_q
 
 #' ### Generar mapa de cuadros sin simbología
